741ff397d0f348efe4f429ceda5f8f10f9ec72f9
Robert den Harink <robert@robhar.com>
2020-02-05 15:36:13 +0100

    ?

--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,32 @@
+use serde::{Serialize, Deserialize};
+use tiny_http::{Server, Response};
+use std::str::FromStr;
+
+#[derive(Serialize, Deserialize, Debug)]
+#[serde(tag = "kind", content = "value")]
+enum Instruction {
+    Toggle(usize),
+    Oscillate(usize, usize)
+}
+
 fn main() {
     let server = Server::http("0.0.0.0:8000").unwrap();
 
-    for request in server.incoming_requests() {
+    for mut request in server.incoming_requests() {
         println!("received request! method: {:?}, url: {:?}, headers: {:?}",
                  request.method(),
                  request.url(),
                  request.headers()
         );
 
+        let mut content = String::new();
+        request.as_reader().read_to_string(&mut content).unwrap();
+        let json: Instruction = serde_json::from_str(&content).unwrap();
+        println!("{:?}", json);
+
         let response = Response::from_string("hello world");
         request.respond(response);
     }
 }
 
-use tiny_http::{Server, Response};
 
